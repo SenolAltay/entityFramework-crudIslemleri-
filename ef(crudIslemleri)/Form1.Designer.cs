5e080b2d536f740c10182c43a8970912fdf90425
Şenol Altay <senol.altay@yandex.com>
2023-02-13 23:03:45 +0300

    Add project files.

---
+++ ef(crudIslemleri)/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ef_crudIslemleri_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHamur = new System.Windows.Forms.TextBox();
+            this.txtDomatesSosu = new System.Windows.Forms.TextBox();
+            this.txtMalzemeler = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.txtBuyukluk = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.listbox1 = new System.Windows.Forms.ListBox();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hamur";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Domates Sosu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Malzemeler";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Fİyat";
+            // 
+            // txtHamur
+            // 
+            this.txtHamur.Location = new System.Drawing.Point(93, 26);
+            this.txtHamur.Name = "txtHamur";
+            this.txtHamur.Size = new System.Drawing.Size(100, 20);
+            this.txtHamur.TabIndex = 5;
+            // 
+            // txtDomatesSosu
+            // 
+            this.txtDomatesSosu.Location = new System.Drawing.Point(93, 56);
+            this.txtDomatesSosu.Name = "txtDomatesSosu";
+            this.txtDomatesSosu.Size = new System.Drawing.Size(100, 20);
+            this.txtDomatesSosu.TabIndex = 6;
+            // 
+            // txtMalzemeler
+            // 
+            this.txtMalzemeler.Location = new System.Drawing.Point(93, 82);
+            this.txtMalzemeler.Name = "txtMalzemeler";
+            this.txtMalzemeler.Size = new System.Drawing.Size(100, 20);
+            this.txtMalzemeler.TabIndex = 7;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(93, 115);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 8;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(93, 152);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 20);
+            this.txtFiyat.TabIndex = 9;
+            // 
+            // txtBuyukluk
+            // 
+            this.txtBuyukluk.Location = new System.Drawing.Point(93, 187);
+            this.txtBuyukluk.Name = "txtBuyukluk";
+            this.txtBuyukluk.Size = new System.Drawing.Size(100, 20);
+            this.txtBuyukluk.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Büyüklük";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(103, 228);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(81, 29);
+            this.btnKaydet.TabIndex = 12;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // listbox1
+            // 
+            this.listbox1.FormattingEnabled = true;
+            this.listbox1.Location = new System.Drawing.Point(230, 26);
+            this.listbox1.Name = "listbox1";
+            this.listbox1.Size = new System.Drawing.Size(267, 225);
+            this.listbox1.TabIndex = 13;
+            this.listbox1.SelectedIndexChanged += new System.EventHandler(this.listbox1_SelectedIndexChanged);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(6, 229);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(81, 28);
+            this.btnGuncelle.TabIndex = 14;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(103, 272);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(81, 28);
+            this.btnsil.TabIndex = 15;
+            this.btnsil.Text = "Kayıt Sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 322);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.listbox1);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtBuyukluk);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtMalzemeler);
+            this.Controls.Add(this.txtDomatesSosu);
+            this.Controls.Add(this.txtHamur);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtHamur;
+        private System.Windows.Forms.TextBox txtDomatesSosu;
+        private System.Windows.Forms.TextBox txtMalzemeler;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.TextBox txtBuyukluk;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.ListBox listbox1;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnsil;
+    }
+}
+
---
+++ ef(crudIslemleri)/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ef_crudIslemleri_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void listBoxEkle(ListBox lstbox,Pizza pizza)
+        {
+            lstbox.Items.Add(pizza.adı+" "+pizza.fiyati);
+        }
+        static List<Pizza> pizzalariGetir()
+        {
+            PizzaDBEntities veritabani = new PizzaDBEntities();
+            return veritabani.Pizzas.ToList();
+        }
+        static void listBoxDoldur(List<Pizza> pizzalar,ListBox listBox)
+        {
+            foreach (var item in pizzalar)
+            {
+                listBox.Items.Add(item.adı + " " + item.fiyati);
+            }
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            PizzaDBEntities veritabani = new PizzaDBEntities();
+            Pizza pizza = new Pizza();
+            pizza.adı = txtAd.Text;
+            pizza.fiyati = Convert.ToDecimal(txtFiyat.Text);
+            pizza.hamurTipi = txtHamur.Text;
+            pizza.boyutu = txtBuyukluk.Text;
+            pizza.Malzemeler = txtMalzemeler.Text;
+            pizza.domatesSosluMu = Convert.ToBoolean(txtDomatesSosu.Text);
+
+            veritabani.Pizzas.Add(pizza);
+            veritabani.SaveChanges();
+            listBoxEkle(listbox1,pizza);
+            pizzalariGetir();
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBoxDoldur(pizzalariGetir(), listbox1);
+        }
+
+        Pizza arananBilgi;
+        PizzaDBEntities veritabani = new PizzaDBEntities();
+
+        private void listbox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtAd.Enabled = false;
+            string secilen=listbox1.SelectedItem.ToString();
+            string pizzaAdi=secilen.Split(' ')[0];
+            
+            PizzaDBEntities veritabani = new PizzaDBEntities();
+            arananBilgi=veritabani.Pizzas.Where(p=> p.adı==pizzaAdi).FirstOrDefault();
+            txtAd.Text = arananBilgi.adı;
+            txtHamur.Text = arananBilgi.hamurTipi;
+            txtBuyukluk.Text = arananBilgi.boyutu;
+            txtDomatesSosu.Text = arananBilgi.domatesSosluMu.ToString();
+            txtFiyat.Text = arananBilgi.fiyati.ToString();
+            txtMalzemeler.Text = arananBilgi.Malzemeler;
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            Pizza pizza = veritabani.Pizzas.Where(p => p.adı == arananBilgi.adı).FirstOrDefault();
+
+            pizza.adı = txtAd.Text;
+            pizza.fiyati = Convert.ToDecimal(txtFiyat.Text);
+            pizza.boyutu = txtBuyukluk.Text;
+            pizza.domatesSosluMu = Convert.ToBoolean(txtDomatesSosu.Text);
+            pizza.hamurTipi = txtHamur.Text;
+            pizza.Malzemeler = txtMalzemeler.Text;
+            veritabani.SaveChanges();
+            MessageBox.Show("güncellendi :)");
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            Pizza pizza = veritabani.Pizzas.Where(p => p.adı == arananBilgi.adı).FirstOrDefault();
+            veritabani.Pizzas.Remove(pizza);
+            veritabani.SaveChanges();
+            MessageBox.Show("Silindi :");
+        }
+    }
+}
